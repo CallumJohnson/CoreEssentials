4f174a3729b6a70aaf6a5c3cfe2342a341c50fc4
Luligabi1 <luligabi111@gmail.com>
2020-11-27 23:58:14 -0300

    Added the ability to see other player's /ping.

--- src/main/java/me/luligabi/coreessentials/command/PingCommand.java
+++ src/main/java/me/luligabi/coreessentials/command/PingCommand.java
@@ -3,12 +3,14 @@ package me.luligabi.coreessentials.command;
 import me.luligabi.coreessentials.CoreEssentials;
 import me.luligabi.coreessentials.utils.MessageUtils;
 import me.luligabi.coreessentials.utils.Permissions;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import sun.plugin2.message.Message;
 
 import java.lang.reflect.InvocationTargetException;
 
@@ -28,11 +30,29 @@ public class PingCommand implements CommandExecutor {
             p.sendMessage(MessageUtils.permissionMessage(Permissions.COMMAND_PING));
             return true;
         }
-        p.sendMessage(MessageUtils.successMessage(prefix, cfg.getString("pingSuccess")
-                .replace("%ping%", Integer.toString(getPing(p)))));
-        return false; //TODO: Add ability to see other player's ping.
+        if(args.length == 0) {
+            p.sendMessage(MessageUtils.successMessage(prefix, cfg.getString("pingSuccess")
+                    .replace("%ping%", Integer.toString(getPing(p)))));
+        }
+        if(args.length == 1) {
+            if(!p.hasPermission(Permissions.COMMAND_PING_OTHERS)) {
+                p.sendMessage(MessageUtils.permissionMessage(Permissions.COMMAND_PING_OTHERS));
+            }
+            Player target = Bukkit.getPlayerExact(args[0]);
+            if(target != null) {
+                p.sendMessage(MessageUtils.successMessage(prefix, cfg.getString("healSuccessOther")
+                        .replace("%target%", target.getDisplayName()
+                        .replace("%ping%", Integer.toString(getPing(target))))));
+            } else {
+                p.sendMessage(MessageUtils.errorMessage(prefix, cfg.getString("targetNotOnline")));
+            }
+        }
+        if(args.length >= 2) {
+            p.sendMessage(MessageUtils.errorMessage(prefix, cfg.getString("pingIncorrectUsage")));
+        }
+        return false;
     }
-    public static int getPing(Player player) {
+    public int getPing(Player player) {
         try {
             Object entityPlayer = player.getClass().getMethod("getHandle").invoke(player);
             return (int) entityPlayer.getClass().getField("ping").get(entityPlayer);
--- src/main/java/me/luligabi/coreessentials/utils/Permissions.java
+++ src/main/java/me/luligabi/coreessentials/utils/Permissions.java
@@ -12,6 +12,7 @@ public class Permissions {
     public static String COMMAND_HEAL_OTHERS = "coreessentials.command.heal.others";
     public static String COMMAND_NIGHT = "coreeessentials.command.night";
     public static String COMMAND_PING = "coreessentials.command.ping";
+    public static String COMMAND_PING_OTHERS = "coreessentials.command.ping.others";
     public static String COMMAND_RAIN = "coreesssentials.command.rain";
     public static String COMMAND_SUN = "coressentials.command.sun";
     public static String COMMAND_THUNDER = "coreessentials.command.thunder";
